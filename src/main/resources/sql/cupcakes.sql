a8ec6609d1a366353be7881063abffcd588cedae
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-04-03 11:41:38 +0200

    Insert all the data in the sql code

--- src/main/resources/sql/cupcakes.sql
+++ src/main/resources/sql/cupcakes.sql
@@ -64,4 +64,25 @@ ALTER TABLE IF EXISTS public.orderline
        ON DELETE NO ACTION
     NOT VALID;
 
+-- Inserting data into bottoms table
+INSERT INTO public.bottoms (flavor, price)
+VALUES ('Chocolate', 5.00),
+       ('Vanilla', 5.00),
+       ('Nutmeg', 5.00),
+       ('Pistachio', 6.00),
+       ('Almond', 7.00);
+
+-- Inserting data into toppings table
+INSERT INTO public.toppings (flavor, price)
+VALUES ('Chocolate', 5.00),
+       ('Blueberry', 5.00),
+       ('Raspberry', 5.00),
+       ('Crispy', 6.00),
+       ('Strawberry', 6.00),
+       ('Rum/Raisin', 7.00),
+       ('Orange', 8.00),
+       ('Lemon', 8.00),
+       ('Blue cheese', 9.00);
+
+
 END;

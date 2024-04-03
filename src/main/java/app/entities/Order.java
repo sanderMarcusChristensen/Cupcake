09fb69bb565dd128be9fbf0b0318939b9dbc4dc4
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-04-03 14:05:48 +0200

    Entities made: Order

---
+++ src/main/java/app/entities/Order.java
@@ -0,0 +1,57 @@
+package app.entities;
+
+public class Order {
+    private int order_id;
+    private int total_price;
+    private String user_name;
+    private int orderline_amount;
+
+    public Order(int order_id, int total_price, String user_name, int orderline_amount) {
+        this.order_id = order_id;
+        this.total_price = total_price;
+        this.user_name = user_name;
+        this.orderline_amount = orderline_amount;
+    }
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public int getTotal_price() {
+        return total_price;
+    }
+
+    public void setTotal_price(int total_price) {
+        this.total_price = total_price;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public int getOrderline_amount() {
+        return orderline_amount;
+    }
+
+    public void setOrderline_amount(int orderline_amount) {
+        this.orderline_amount = orderline_amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "order_id=" + order_id +
+                ", total_price=" + total_price +
+                ", user_name='" + user_name + '\'' +
+                ", orderline_amount=" + orderline_amount +
+                '}';
+    }
+}

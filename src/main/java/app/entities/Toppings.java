5ddd7090dfc65d9ff3e70ecdd47cb9ad389b7f74
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-04-03 14:06:10 +0200

    Entities made: Toppings

---
+++ src/main/java/app/entities/Toppings.java
@@ -0,0 +1,46 @@
+package app.entities;
+
+public class Toppings {
+    private int topping_id;
+    private String flavor;
+    private int price;
+
+    public Toppings(int topping_id, String flavor, int price) {
+        this.topping_id = topping_id;
+        this.flavor = flavor;
+        this.price = price;
+    }
+
+    public int getTopping_id() {
+        return topping_id;
+    }
+
+    public void setTopping_id(int topping_id) {
+        this.topping_id = topping_id;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Toppings{" +
+                "topping_id=" + topping_id +
+                ", flavor='" + flavor + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}

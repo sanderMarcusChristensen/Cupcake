6b80667bd54d2610da2e7accca7c6844dfaa93fc
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-04-03 14:05:36 +0200

    Entities made: Bottoms

---
+++ src/main/java/app/entities/Bottoms.java
@@ -0,0 +1,46 @@
+package app.entities;
+
+public class Bottoms {
+    private int bottom_id;
+    private String flavor;
+    private int price;
+
+    public Bottoms(int bottom_id, String flavor, int price) {
+        this.bottom_id = bottom_id;
+        this.flavor = flavor;
+        this.price = price;
+    }
+
+    public int getBottom_id() {
+        return bottom_id;
+    }
+
+    public void setBottom_id(int bottom_id) {
+        this.bottom_id = bottom_id;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Bottoms{" +
+                "bottom_id=" + bottom_id +
+                ", flavor='" + flavor + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}

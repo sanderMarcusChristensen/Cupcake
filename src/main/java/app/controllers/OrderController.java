f868615059994ff62e5ea8eb01615598791671e9
MateenCPH <cph-mr645@cphbusiness.dk>
2024-04-04 14:27:50 +0200

    beginning of ordercontroller

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,4 +1,18 @@
 package app.controllers;
 
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+import app.persistence.UserMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
 public class OrderController {
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.post("addtobasket", ctx -> addtobasket(ctx, connectionPool));
+
+    }
+
+    private static void addtobasket(Context ctx, ConnectionPool connectionPool) {
+
+    }
 }

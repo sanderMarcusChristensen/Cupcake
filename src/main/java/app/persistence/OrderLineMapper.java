9a9a1a118a60259d87cd8db88716788b822357db
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-04-04 13:05:44 +0200

    OrderLineMapper metoder: getAllOrderLines, getOrderLineById, addOrderLine, removeOrderLineById

--- src/main/java/app/persistence/OrderLineMapper.java
+++ src/main/java/app/persistence/OrderLineMapper.java
@@ -1,4 +1,104 @@
 package app.persistence;
 
+
+import app.entities.Order;
+import app.entities.Orderline;
+import app.exceptions.DatabaseException;
+
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderLineMapper {
+
+    private static List<Orderline> orderLineList = new ArrayList<>();
+
+    public static List<Orderline> getAllOrderLines(ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "select * from orderline;";
+
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    int orderline_id = rs.getInt("orderline_id");
+                    int order_id = rs.getInt("order_id");
+                    int total_price = rs.getInt("total_price");
+                    int topping_id = rs.getInt("topping_id");
+                    int bottom_id = rs.getInt("bottom_id");
+
+                    orderLineList.add(new Orderline(orderline_id, order_id, total_price, topping_id, bottom_id));
+                }
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Fejl i tilgangen til databasen", e.getMessage());
+        }
+        return orderLineList;
+    }
+
+    public static Orderline getOrderLineById(int ord_id, ConnectionPool connectionPool) throws DatabaseException {
+
+        String sql = "select * from orderline WHERE order_id = ?;";
+        Orderline cupCakeOrder = null;
+
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+
+
+                ps.setInt(1, ord_id);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    int orderline_id = rs.getInt("orderline_id");
+                    int order_id = rs.getInt("order_id");
+                    int total_price = rs.getInt("total_price");
+                    int topping_id = rs.getInt("topping_id");
+                    int bottom_id = rs.getInt("bottom_id");
+
+                    cupCakeOrder = new Orderline(orderline_id, order_id, total_price, topping_id, bottom_id);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Fejl i tilgangen til databasen", e.getMessage());
+        }
+        return cupCakeOrder;
+    }
+
+    public static boolean addOrderLine(int orderline_id, int total_price, int topping_id, int bottom_id, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "INSERT INTO orderline (orderline_id, total_price, topping_id, bottom_id) VALUES (?,?,?,?); ";
+        boolean retBool = false;
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setInt(1, orderline_id);
+                ps.setInt(2, total_price);
+                ps.setInt(3, topping_id);
+                ps.setInt(4, bottom_id);
+                int rowsAffected = ps.executeUpdate();  //what
+                if (rowsAffected == 1) {
+                    retBool = true;
+                }
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Fejl i tilgangen til databasen", e.getMessage());
+        }
+        return retBool;
+    }
+
+    public static void removeOrderLineById(int orderline_id, ConnectionPool connectionPool) throws DatabaseException {
+
+        String sql = "DELETE FORM orderline WHERE orderline_id = ?";
+
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setInt(1, orderline_id);
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Fejl i tilgangen til databasen", e.getMessage());
+        }
+
+
+    }
 }

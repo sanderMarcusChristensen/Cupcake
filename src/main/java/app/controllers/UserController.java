d9b18d731c1b77f27863ee416c2806a135a48398
MateenCPH <cph-mr645@cphbusiness.dk>
2024-04-07 13:13:08 +0200

    moved addtobasket route from usercontroller to ordercontroller

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -13,67 +13,49 @@ import io.javalin.http.Context;
 
 import java.util.List;
 
-public class UserController
-{
-    public static void addRoutes(Javalin app, ConnectionPool connectionPool)
-    {
+public class UserController {
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.post("login", ctx -> login(ctx, connectionPool));
         app.get("logout", ctx -> logout(ctx));
         app.get("createuser", ctx -> ctx.render("createuser.html"));
         app.post("createuser", ctx -> createUser(ctx, connectionPool));
-        app.post("addtobasket", ctx -> addToBasket(ctx));
     }
 
-    private static void addToBasket(Context ctx) {
-
-    }
-
-    private static void createUser(Context ctx, ConnectionPool connectionPool)
-    {
+    private static void createUser(Context ctx, ConnectionPool connectionPool) {
         // Hent form parametre
         String username = ctx.formParam("username");
         String password1 = ctx.formParam("password1");
         String password2 = ctx.formParam("password2");
 
-        if (password1.equals(password2))
-        {
-            try
-            {
+        if (password1.equals(password2)) {
+            try {
                 UserMapper.createuser(username, password1, connectionPool);
                 ctx.attribute("message", "Du er hermed oprettet med brugernavn: " + username +
                         ". Nu skal du logge på.");
                 ctx.render("index.html");
-            }
-
-            catch (DatabaseException e)
-            {
+            } catch (DatabaseException e) {
                 ctx.attribute("message", "Dit brugernavn findes allerede. Prøv igen, eller log ind");
                 ctx.render("createuser.html");
             }
-        } else
-        {
+        } else {
             ctx.attribute("message", "Dine to passwords matcher ikke! Prøv igen");
             ctx.render("createuser.html");
         }
-
     }
 
-    private static void logout(Context ctx)
-    {
+    private static void logout(Context ctx) {
         ctx.req().getSession().invalidate();
         ctx.redirect("/");
     }
 
 
-    public static void login(Context ctx, ConnectionPool connectionPool)
-    {
+    public static void login(Context ctx, ConnectionPool connectionPool) {
         // Hent form parametre
         String username = ctx.formParam("username");
         String password = ctx.formParam("password");
 
         // Check om bruger findes i DB med de angivne username + password
-        try
-        {
+        try {
             User user = UserMapper.login(username, password, connectionPool);
             ctx.sessionAttribute("currentUser", user);
             // Hvis ja, send videre til forsiden med login besked
@@ -82,11 +64,10 @@ public class UserController
             ctx.attribute("bottomsList", bottomsList);
             ctx.attribute("toppingsList", toppingsList);
             ctx.render("orderpage.html");
-        }
-        catch (DatabaseException e)
-        {
+
+        } catch (DatabaseException e) {
             // Hvis nej, send tilbage til login side med fejl besked
-            ctx.attribute("message", e.getMessage() );
+            ctx.attribute("message", e.getMessage());
             ctx.render("index.html");
         }
 

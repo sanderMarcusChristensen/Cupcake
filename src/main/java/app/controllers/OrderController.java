9b71d24d368d1fe7b6d39caaf714556573ce52f2
MateenCPH <cph-mr645@cphbusiness.dk>
2024-04-07 13:11:44 +0200

    addtobasket function

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,38 +1,49 @@
 package app.controllers;
 
+import app.entities.Bottoms;
+import app.entities.Orderline;
+import app.entities.Toppings;
 import app.entities.User;
 import app.exceptions.DatabaseException;
-import app.persistence.ConnectionPool;
-import app.persistence.UserMapper;
+import app.persistence.*;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderController {
+    protected static List<Orderline> basket = new ArrayList<>();
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
-        //app.post("addtobasket", ctx -> addtobasket(ctx, connectionPool));
+        app.post("addtobasket", ctx -> addtobasket(ctx, connectionPool));
 
     }
 
-    /*private static void addtobasket(Context ctx, ConnectionPool connectionPool) {
-        String topping = ctx.formParam("topping");
+    private static void addtobasket(Context ctx, ConnectionPool connectionPool) {
+
+        int bottomId = Integer.parseInt(ctx.formParam("bottom_id"));
+        int toppingId = Integer.parseInt(ctx.formParam("topping_id"));
+        int amount = Integer.parseInt(ctx.formParam("amount"));
         User user = ctx.sessionAttribute("currentUser");
 
         try {
-            if (taskname.length() > 3) {
-                Task newTask = TaskMapper.addTask(user, taskname, connectionPool);
+            if (bottomId != 0 && toppingId != 0 && amount > 0) {
+                for (int i = 0; i < amount; i++){
+                    OrderLineMapper.addOrderLine(toppingId, bottomId, connectionPool);
+                }
             } else {
-                ctx.attribute("message", "En task skal være længere end 3 tegn!");
+                ctx.attribute("message", "Please choose correct inputs");
             }
-            List<Task> taskList = TaskMapper.getAllTasksPerUser(user.getUserId(), connectionPool);
-            ctx.attribute("taskList", taskList);
-            ctx.render("task.html");
+            List<Bottoms> bottomsList = BottomsMapper.getAllBottoms(connectionPool);
+            List<Toppings> toppingsList = ToppingsMapper.getAllToppings(connectionPool);
+            ctx.attribute("bottomsList", bottomsList);
+            ctx.attribute("toppingsList", toppingsList);
+            ctx.render("orderpage.html");
 
         } catch (DatabaseException e) {
             ctx.attribute("message", "Noget gik galt. Prøv evt. igen");
-            ctx.render("task.html");
+            ctx.render("orderpage.html");
         }
-    }*/
+    }
 }
 

ca7a67306bc330487258937840b5bbbecb941ec7
MateenCPH <cph-mr645@cphbusiness.dk>
2024-04-08 13:02:59 +0200

    added cartline class

---
+++ src/main/java/app/entities/CartLine.java
@@ -0,0 +1,31 @@
+package app.entities;
+
+public class CartLine {
+
+    private Toppings toppings;
+    private Bottoms bottoms;
+    private int amount;
+
+    public CartLine(Toppings toppings, Bottoms bottoms, int amount) {
+        this.toppings = toppings;
+        this.bottoms = bottoms;
+        this.amount = amount;
+    }
+
+    public Toppings getToppings() {
+        return toppings;
+    }
+
+    public Bottoms getBottoms() {
+        return bottoms;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getPrice()
+    {
+        return (toppings.getPrice() + bottoms.getPrice()) * amount;
+    }
+}

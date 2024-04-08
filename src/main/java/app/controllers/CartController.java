da29cad9972e27a924a0658e73cbc05b1f59b03f
MateenCPH <cph-mr645@cphbusiness.dk>
2024-04-08 13:02:50 +0200

    added cartcontroller class

---
+++ src/main/java/app/controllers/CartController.java
@@ -0,0 +1,55 @@
+package app.controllers;
+
+import app.entities.*;
+import app.exceptions.DatabaseException;
+import app.persistence.BottomsMapper;
+import app.persistence.ConnectionPool;
+import app.persistence.ToppingsMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.List;
+
+public class CartController {
+
+    Cart cart;
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.post("addtobasket", ctx -> addtobasket(ctx, connectionPool));
+
+    }
+
+    private static void addtobasket(Context ctx, ConnectionPool connectionPool) {
+
+        int bottomId = Integer.parseInt(ctx.formParam("bottom_id"));
+        int toppingId = Integer.parseInt(ctx.formParam("topping_id"));
+        int amount = Integer.parseInt(ctx.formParam("amount"));
+        User user = ctx.sessionAttribute("currentUser");
+        Cart cart = ctx.sessionAttribute("cart");
+
+        if (cart == null) {
+            cart = new Cart();
+        }
+
+        try {
+            if (bottomId != 0 && toppingId != 0 && amount > 0) {
+                Bottoms bottoms = BottomsMapper.getBottomsById(bottomId, connectionPool);
+                Toppings toppings = ToppingsMapper.getToppingsById(toppingId, connectionPool);
+
+                cart.add(toppings, bottoms, amount);
+                ctx.sessionAttribute("cart", cart);
+
+            } else {
+                ctx.attribute("message", "Please choose correct inputs");
+            }
+            List<Bottoms> bottomsList = BottomsMapper.getAllBottoms(connectionPool);
+            List<Toppings> toppingsList = ToppingsMapper.getAllToppings(connectionPool);
+            ctx.attribute("bottomsList", bottomsList);
+            ctx.attribute("toppingsList", toppingsList);
+            ctx.render("orderpage.html");
+        } catch (DatabaseException e) {
+            ctx.attribute("message", "Noget gik galt. Prøv evt. igen");
+            ctx.render("orderpage.html");
+
+        }
+    }
+}

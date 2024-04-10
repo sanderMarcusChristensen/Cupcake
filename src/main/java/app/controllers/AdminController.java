bb59938ee9c2b9d5d9a1c94f5dc0ee4e86735f86
MateenCPH <cph-mr645@cphbusiness.dk>
2024-04-10 09:57:20 +0200

    added backtohomepage method and routing

--- src/main/java/app/controllers/AdminController.java
+++ src/main/java/app/controllers/AdminController.java
@@ -1,12 +1,35 @@
 package app.controllers;
 
+import app.entities.Bottoms;
+import app.entities.Toppings;
+import app.entities.User;
+import app.exceptions.DatabaseException;
+import app.persistence.BottomsMapper;
 import app.persistence.ConnectionPool;
+import app.persistence.ToppingsMapper;
+import app.persistence.UserMapper;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("viewOrder", ctx -> ctx.render("viewOrder.html"));
         app.get("userViewPage", ctx -> ctx.render("userViewPage.html"));
-        app.get("homepage", ctx -> ctx.render("orderpage.html"));
+        app.get("backToHomepage", ctx -> backToHomepage(ctx, connectionPool));
+    }
+
+    private static void backToHomepage(Context ctx, ConnectionPool connectionPool) {
+        List<Bottoms> bottomsList = ctx.sessionAttribute("bottomsList");
+        List<Toppings> toppingsList = ctx.sessionAttribute("toppingsList");
+        ctx.attribute("bottomsList", bottomsList);
+        ctx.attribute("toppingsList", toppingsList);
+        ctx.render("orderpage.html");
+
     }
 }
+
+
+

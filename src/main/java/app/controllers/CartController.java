ca4e9fb4308f948a773c91a7e2e52ab333572ac4
MateenCPH <cph-mr645@cphbusiness.dk>
2024-04-10 09:57:37 +0200

    code cleanup and refactoring

--- src/main/java/app/controllers/CartController.java
+++ src/main/java/app/controllers/CartController.java
@@ -8,6 +8,7 @@ import app.persistence.ToppingsMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CartController {
@@ -17,53 +18,24 @@ public class CartController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.post("addtobasket", ctx -> addtobasket(ctx, connectionPool));
         app.get("viewbasket", ctx -> ctx.render("checkOutPage.html"));
-        //app.get("orderpage", ctx -> ctx.render("orderpage.html"));
-        app.get("orderpage", ctx -> loadOrderPage(ctx, connectionPool));
     }
 
-    private static void loadOrderPage(Context ctx, ConnectionPool connectionPool) {
-        User user = ctx.sessionAttribute("currentUser");
-
-        List<Bottoms> bottomsList = null;
-        List<Toppings> toppingsList = null;
-        try {
-            bottomsList = BottomsMapper.getAllBottoms(connectionPool);
-            toppingsList = ToppingsMapper.getAllToppings(connectionPool);
-        } catch (DatabaseException e) {
-            ctx.attribute("message", e.getMessage());
-        }
-        ctx.attribute("currentUserName", user.getUser_name());
-        ctx.attribute("bottomsList", bottomsList);
-        ctx.attribute("toppingsList", toppingsList);
-        ctx.render("orderpage.html");
-    }
 
     private static void addtobasket(Context ctx, ConnectionPool connectionPool) {
-
-        List<Bottoms> bottomsList = null;
-        List<Toppings> toppingsList = null;
 
         int bottomId = Integer.parseInt(ctx.formParam("bottom_id"));
         int toppingId = Integer.parseInt(ctx.formParam("topping_id"));
         int amount = Integer.parseInt(ctx.formParam("amount"));
+
         User user = ctx.sessionAttribute("currentUser");
         Cart cart = ctx.sessionAttribute("cart");
+
+        List<Bottoms> bottomsList = ctx.sessionAttribute("bottomsList");
+        List<Toppings> toppingsList = ctx.sessionAttribute("toppingsList");
 
         if (cart == null) {
             cart = new Cart();
         }
-
-        if (bottomsList == null || toppingsList == null) {
-            try {
-                bottomsList = BottomsMapper.getAllBottoms(connectionPool);
-                toppingsList = ToppingsMapper.getAllToppings(connectionPool);
-            } catch (DatabaseException e) {
-                ctx.attribute("message", e.getMessage());
-            }
-            ctx.attribute("bottomsList", bottomsList);
-            ctx.attribute("toppingsList", toppingsList);
-        }
-
 
         try {
             if (bottomId != 0 && toppingId != 0 && amount > 0) {
@@ -76,11 +48,14 @@ public class CartController {
             } else {
                 ctx.attribute("message", "Please choose correct inputs");
             }
-
+            ctx.attribute("bottomsList", bottomsList);
+            ctx.attribute("toppingsList", toppingsList);
             ctx.render("orderpage.html");
 
         } catch (DatabaseException e) {
             ctx.attribute("message", "Noget gik galt. Prøv evt. igen");
+            ctx.attribute("bottomsList");
+            ctx.attribute("toppingsList");
             ctx.render("orderpage.html");
 
         }

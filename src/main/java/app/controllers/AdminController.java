6d30e0dbd174f8b98d5173a367bcc2bbcb2f63d8
MateenCPH <cph-mr645@cphbusiness.dk>
2024-04-11 10:40:44 +0200

    added and finished viewOrder method

--- src/main/java/app/controllers/AdminController.java
+++ src/main/java/app/controllers/AdminController.java
@@ -1,13 +1,11 @@
 package app.controllers;
 
 import app.entities.Bottoms;
+import app.entities.Order;
 import app.entities.Toppings;
 import app.entities.User;
 import app.exceptions.DatabaseException;
-import app.persistence.BottomsMapper;
-import app.persistence.ConnectionPool;
-import app.persistence.ToppingsMapper;
-import app.persistence.UserMapper;
+import app.persistence.*;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -16,9 +14,22 @@ import java.util.List;
 
 public class AdminController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
-        app.get("viewOrder", ctx -> ctx.render("viewOrder.html"));
-        app.get("userViewPage", ctx -> ctx.render("userViewPage.html"));
+        app.get("viewOrders", ctx -> viewOrders(ctx, connectionPool));
+        app.get("viewUsers", ctx -> ctx.render("viewUsers.html"));
         app.get("backToHomepage", ctx -> backToHomepage(ctx, connectionPool));
+    }
+
+    private static void viewOrders(Context ctx, ConnectionPool connectionPool) {
+        try {
+            List<Order> orderList = OrderMapper.getAllOrder(connectionPool);
+
+            ctx.attribute("orderList", orderList);
+            ctx.render("viewOrders.html");
+        } catch (DatabaseException e) {
+            ctx.attribute("message", e.getMessage());
+            ctx.render("viewOrders.html");
+        }
+
     }
 
     private static void backToHomepage(Context ctx, ConnectionPool connectionPool) {
@@ -30,6 +41,3 @@ public class AdminController {
 
     }
 }
-
-
-
